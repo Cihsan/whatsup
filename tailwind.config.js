94c98666b98e159c017b4d7cc9b60ff91346c7ac
Cihsan <mutamuty@gmail.com>
2022-05-30 19:51:10 +0600

    login page

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#f43f5e",
+
+          "secondary": "#f76c65",
+
+          "accent": "#3D4451",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+}
